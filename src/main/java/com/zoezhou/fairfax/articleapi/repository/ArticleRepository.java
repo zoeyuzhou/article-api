5daacf64e8fd1e03f669ee17c660ada84f366d9d
Zoe Zhou <zhouyu.zoey@gmail.com>
2018-07-07 11:13:09 +1000

    Initial commit.

--- src/main/java/com/zoezhou/fairfax/articleapi/repository/ArticleRepository.java
+++ src/main/java/com/zoezhou/fairfax/articleapi/repository/ArticleRepository.java
@@ -8,10 +8,4 @@ import java.util.Optional;
 
 public interface ArticleRepository extends JpaRepository<Article, String>, ArticleRepositoryCustom {
 
-/*
-    @Override
-    Optional<Article> findById(String id);
-
-    @Override
-    List<Article> findAll(); */
 }
